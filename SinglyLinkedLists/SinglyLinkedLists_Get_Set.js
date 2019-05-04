56a342e88de7e837c84f5999753359175c3ff648
Tinedanzer <johnhillallen@gmail.com>
2019-05-03 20:15:06 -0500

    added a set function wuwu

--- SinglyLinkedLists/SinglyLinkedLists_Get_Set.js
+++ SinglyLinkedLists/SinglyLinkedLists_Get_Set.js
@@ -1,4 +1,5 @@
 // "GET" takes an index(number) and returns the item in that position.
+// "SET" takes a value and an index; finds the index and replaces the value.
 class Node{
     constructor(val){
         this.value=val;
@@ -30,10 +31,19 @@ class SinglyLinkedList{
         let current=this.head
         for (let x=0;x<this.length;x++){
             if(i===x) {
-                console.log(current.value);
-                return current.value;}
+                // console.log(current);
+                return current;}
             current=current.next;            
         }
+    }
+// "SET" takes a value and an index; finds the index and replaces the value. Will use the 'GET' method.
+    set(value,i){
+        let foundNode=this.get(i);
+        if(foundNode){
+            foundNode.value= value;
+            return true;
+        }
+        return false;
     }
 }
 let newList = new SinglyLinkedList();
@@ -43,4 +53,5 @@ newList.push('aroo5zz');
 newList.push('aroo6q');
 newList.push('aroo3sdfdf');
 newList.get(4);
-// console.log(newList);
+newList.set(433,1);
+console.log(newList);
