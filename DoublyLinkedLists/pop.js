775600d3541b03a412d6b3bf3b391e6883b6b05e
Tinedanzer <johnhillallen@gmail.com>
2019-05-07 21:21:40 -0500

    pop pop

--- DoublyLinkedLists/pop.js
+++ DoublyLinkedLists/pop.js
@@ -18,18 +18,24 @@ class DoublyLinkedLists{
         this.length++;
         return this;}
     pop(){
-        let aroo=this.tail.value;
+        let aroo=this.tail;
         if(this.length===0)return undefined;
         if(this.length===1){this.head=null;this.tail=null;this.length--;return aroo}
-        
+// reassign new tail to the previous of the last tail, then reassigning  the new tail's next to null
+        this.tail=aroo.prev;
+        this.tail.next=null;
+// severing the link from both sides; the Node returned will have no 'prev' or 'next links.
+        aroo.prev=null;
         this.length--;
+        console.log(aroo);
         return aroo;
     }
 }
 const newList= new DoublyLinkedLists;
 newList.push('Aro1o');
-// newList.push('aroo2');
-// newList.push('aroo3sdfdf');
+newList.push('aroo2');
+newList.push('aroo3sdfdf');
 // newList.push('aroo4zz');
+newList.pop();
 newList.pop();
 console.log(newList);
