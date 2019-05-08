bf5a9a39f92924d827f5bf89dc47063e2dcb6caa
Tinedanzer <johnhillallen@gmail.com>
2019-05-07 22:28:53 -0500

    add one to start of doubly linked list!

---
+++ DoublyLinkedLists/unshift.js
@@ -0,0 +1,36 @@
+class Node{
+    constructor(val){
+        this.value=val;
+        this.next=null;
+        this.prev=null;
+    }
+}
+class DoublyLinkedLists{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0;
+    }
+    push(val){
+        let newNode=new Node(val);
+        if(!this.head){this.head=newNode;this.tail=newNode;}
+        else{this.tail.next=newNode;newNode.prev=this.tail;this.tail=newNode;}
+        this.length++;
+        return this;}
+    unshift(val){
+        let newNode=new Node(val);
+        let aroo=this.head;
+        this.head.prev=newNode;
+        this.head=newNode;
+        this.head.next=aroo;
+        this.length++;
+        return this;
+    }
+}
+const newList= new DoublyLinkedLists;
+newList.push('Aro1o');
+newList.push('aroo2');
+newList.push('aroo3sdfdf');
+// newList.push('aroo4zz');
+newList.unshift('mr.hanky');
+console.log(newList);
