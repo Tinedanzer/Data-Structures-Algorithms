0184bf8322ef8a7b9e88b7bcd3bb83734eae89f9
Tinedanzer <johnhillallen@gmail.com>
2019-05-13 01:19:09 -0500

    pew pew!

---
+++ Binary_Search_tree/insert_BST.js
@@ -0,0 +1,44 @@
+class Node{
+    constructor(val){
+        this.value=val;
+        this.left=null;
+        this.right=null;
+    }
+}
+class BST{
+    constructor(){
+        this.root=null;
+    }
+    insert(num){
+        let newNode= new Node(num);
+        if(this.root===null){this.root=newNode;return this;}
+        let pewpew=this.root;
+        while(1){
+            if(num===pewpew.value)return undefined;
+            if(num<pewpew.value){
+                if(pewpew.left===null){
+                    pewpew.left=newNode;
+                    return this;
+                }
+                else pewpew=pewpew.left;
+            }
+            else{
+                if(pewpew.right===null){
+                    pewpew.right=newNode;
+                    return this;
+                }
+                else pewpew=pewpew.right;
+            }
+        }
+    return this;
+    }
+}
+let aroo= new BST();
+aroo.insert(10);
+aroo.insert(5);
+aroo.insert(13);
+aroo.insert(13);
+aroo.insert(20);
+aroo.insert(0);
+aroo.insert(-5);
+console.log(aroo.root);
