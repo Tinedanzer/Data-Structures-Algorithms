e6ba3ddce9a71d739680eb868c030f5f48c879ef
Tinedanzer <johnhillallen@gmail.com>
2019-05-14 15:58:24 -0500

    completed

--- Tree_Traversal/Depth_First_Search_PreOrder.js
+++ Tree_Traversal/Depth_First_Search_PreOrder.js
@@ -1,3 +1,5 @@
+//Depth First Search: Visits all nodes on left side, and left side nodes' left side, before
+// going back up and traversing all right sides starting at the youngest child.
 class Node{
     constructor(val){
         this.value=val;
@@ -31,20 +33,7 @@ class BST{
             }
         }
     }
-// an empty array [] returns true in shitty javascript; '' string  returns false however; stupid fun.#madtimewasted
-    breadthFirst(){
-        let data=[];
-        let queue=[];
-        let node=this.root;
-        queue.push(this.root);
-        while(queue.length){
-            node=queue.shift();
-            data.push(node.value);
-            if(node.left) queue.push(node.left);
-            if(node.right) queue.push(node.right);
-        }
-        return data;
-    }
+
 }
 let aroo= new BST();
 aroo.insert(10);
@@ -53,4 +42,3 @@ aroo.insert(13);
 aroo.insert(20);
 aroo.insert(0);
 aroo.insert(6);
-console.log(aroo.breadthFirst());
