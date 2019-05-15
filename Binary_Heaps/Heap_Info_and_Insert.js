0ff74595ec6ab646fe95ef96ba696c1c87c36637
Tinedanzer <johnhillallen@gmail.com>
2019-05-15 00:06:02 -0500

    starting binary  heaps

---
+++ Binary_Heaps/Heap_Info_and_Insert.js
@@ -0,0 +1,4 @@
+// only main rule is that the children must be less than the parent; nodes are filled in left to right.
+
+// For Binary Heaps:
+// For an index of an array 'N'..... the left child is stored at 2n+1; the right at 2n+2;
