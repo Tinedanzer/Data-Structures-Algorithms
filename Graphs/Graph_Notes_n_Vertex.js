23cff8dbd7579a68fd3256cd684822966fe11500
Tinedanzer <johnhillallen@gmail.com>
2019-06-10 00:49:12 -0500

    adding notes

--- Graphs/Graph_Notes_n_Vertex.js
+++ Graphs/Graph_Notes_n_Vertex.js
@@ -1,1 +1,8 @@
 // Graph Notes and Vertex Work
+// edge is the connection between two vertices(one-way or two-way)
+// Undirected Graph : vertexes are linked to one another(both-way connection) (facebook)
+// Directed Graph : vertexes now have a one-way direction, that give the link (twitter)
+
+// Weight Graph: : vertexes now have weight in a certain/or either direction to the next vertex
+// think 3d space, linear algebra and difficulty of terrain from one direction to another
+// or back the other way downhill.
