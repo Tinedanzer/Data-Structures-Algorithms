da769b26069acd50b9950a4b2216f316a1574587
Tinedanzer <johnhillallen@gmail.com>
2019-06-11 17:14:23 -0500

    updated notes

--- Graphs/Graphs_Vertex_Edge_Add_Delete.js
+++ Graphs/Graphs_Vertex_Edge_Add_Delete.js
@@ -1,36 +1,36 @@
 // undirected graph(meaning edges go both ways)
 class Graph{
     constructor(){
-        this.adjacenyList= {}
+        this.adjacencyList= {}
     }
     // establishes the vertex before connecting edges; creating new key with a value of empty array
     addVertex(vertex){
-        if(!this.adjacenyList[vertex])
-        this.adjacenyList[vertex]=[]
+        if(!this.adjacencyList[vertex])
+        this.adjacencyList[vertex]=[]
     }
     // creating an undirected graph; meaning edges must go both ways from both vertices.
     addEdge(vertex,connect){
-        this.adjacenyList[vertex].push(connect);
-        this.adjacenyList[connect].push(vertex);
+        this.adjacencyList[vertex].push(connect);
+        this.adjacencyList[connect].push(vertex);
     }
     removeEdge(vertex,connect){
-        // let x= this.adjacenyList[vertex].indexOf(connect);
-        // let y=this.adjacenyList[connect].indexOf(vertex);
-        // this.adjacenyList[vertex].splice(x,1);
-        // this.adjacenyList[connect].splice(y,1);
+        // let x= this.adjacencyList[vertex].indexOf(connect);
+        // let y=this.adjacencyList[connect].indexOf(vertex);
+        // this.adjacencyList[vertex].splice(x,1);
+        // this.adjacencyList[connect].splice(y,1);
 // better method, ^^ above method would splice the array while the for loop is trying to cover it;
 // causing unexpected results. Filtering an array for the values you want or dont want is better
 // in this case.
-        this.adjacenyList[vertex] = this.adjacenyList[vertex].filter(v=> v!==connect)
-        this.adjacenyList[connect] = this.adjacenyList[connect].filter(v=> v!==vertex)
+        this.adjacencyList[vertex] = this.adjacencyList[vertex].filter(v=> v!==connect)
+        this.adjacencyList[connect] = this.adjacencyList[connect].filter(v=> v!==vertex)
 
     }
     removeVertex(vertex){
-        for(let x of this.adjacenyList[vertex]){
+        for(let x of this.adjacencyList[vertex]){
             this.removeEdge(vertex, x)
         }
         // deleting an object Key to completely remove it, after we removed its values.
-        delete this.adjacenyList[vertex]
+        delete this.adjacencyList[vertex]
     }
 }
 let aroo= new Graph();
