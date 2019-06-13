e1a9d49f366823434dac3016662e803a59446ffd
Tinedanzer <johnhillallen@gmail.com>
2019-06-12 23:48:09 -0500

    made notes for quicksort

--- Sorting_Algorithms/QuickSort.js
+++ Sorting_Algorithms/QuickSort.js
@@ -1,5 +1,11 @@
 // time complexity = average: (nlogn); worst: (n^2)
 // space complexity= (logn)
+// Picks a number(say 5), then puts all numbers less of it to the left, greater to the right.
+// Then we recursively do this again for the items left of number(say 5) and all the ones for the right.
+// We go through the array and keep track of how many numbers are less than 5, we move them right after
+// number(say 5)..ex(from 5,18,21,3,4,22) to (5,3,4,18,21,22) then after we finish the loop
+// we swap index[0] which  is  5 with the index[of the amount of numbers that was less than 5
+// [3,4,5,18,21,22]
 function quickIndx(arr, start=0, end= arr.length-1){
     let countSwapIndx=start;
     let pivotIndx=start;
@@ -32,4 +38,4 @@ function recursiveQuickSort(arr, left= 0, right= arr.length-1){
     }
     return arr;
 }
-console.log(recursiveQuickSort([8,10,11,2,4]));
+console.log(recursiveQuickSort([8,10,11,2,4,100,87,55,45]));
